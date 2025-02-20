5677380aa51e55c207d40bfa4d1bc6f29c277b6d
Karen Benites Segura <klbenites96@gmail.com>
2025-02-20 12:40:03 -0500

    done functions and loops exercises

--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -7,20 +7,111 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 
 // 1. Crea un bucle que imprima los números del 1 al 20
 
+for (let i = 1; i<=20; i++){
+    console.log(i)
+}
+
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
+let suma = 0
+
+for (let i = 1; i<=100; i++){
+    console.log(`${i} + ${suma} = ${suma += i}`)
+}
+
+console.log(`Final sum with loop: ${suma}`)
+
+//con while
+suma = 0
+let i = 1
+
+while(i<=100){
+    suma+=i
+    i++
+}
+
+console.log(`Final sum with while: ${suma}`)
 
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+for (let i = 1; i<=50; i++){
+    if (i%2 == 0){
+        console.log(i)
+    }
+}
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
 
+let family = ['Kelvin', 'Nikki', 'Nancy', 'Marcos', 'Sebastian', 'Jean']
+
+for (let familymember of family){
+    console.log(familymember)
+}
+
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+let vowelCount = 0
+let text = 'You are the most beautiful'
+let vowels = new Set(['a','e','i','o','u'])
+i =0
+
+while (i<text.length){
+    if (vowels.has(text[i].toLowerCase())){
+        vowelCount++
+    }
+    i++
+}
+console.log(`my vowel count: ${vowelCount}`)
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+let mynumbers = [4,3,1,2]
+let multiplier = 1
+
+for (let number of mynumbers){
+    console.log(`${multiplier} * ${number} = ${multiplier*=number}`)
+}
+
+console.log(`Final product: ${multiplier}`)
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+let a = 1
+console.log(`Tabla de multiplicar del 5`)
+let mySet = new Set()
+while (a<=12){
+    mySet.add(a*5)
+    a++
+}
+console.log(mySet)
 
 // 8. Usa un bucle para invertir una cadena de texto
+let string = 'IloveMyHusband'
+let counter = string.length
+let invertedString = ""
+
+while(counter>0){
+    invertedString +=string[counter-1]
+    counter--
+}
+
+console.log(`String: ${string}, inverted string: ${invertedString}`)
 
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
+let fibonacciArray = [0, 1]
+let n =2
+
+while (fibonacciArray.length<10){
+    fibonacciArray.push(fibonacciArray[n-1] + fibonacciArray[n-2])
+    n++
+}
+
+console.log(fibonacciArray)
 
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+
+let generalArray = [2,3,4,67,89,10,2,34,0,1,5,6]
+let moreThanTenArray = []
+
+for (let number of generalArray){
+    if (number > 10){
+        moreThanTenArray.push(number)
+    }
+}
+
+console.log(moreThanTenArray)
--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -7,20 +7,152 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 
 // 1. Crea una función que reciba dos números y devuelva su suma
 
+function sum(a =0,b =0){
+    return a+b
+}
+
+let result = sum(2,34)
+console.log(`Result sum: ${result}`)
+
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+let array = [2,3,6,20,7,34,33]
+
+function mayor(array){
+    let mayornum
+    for (let e = 1; e < array.length; e++){
+        if (array[e]>array[e-1]){
+            mayornum = array[e]
+        }
+    }
+    return mayornum
+}
+
+console.log(`El mayor es: ${mayor(array)}`)
 
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
 
+
+let texttest = 'You are the most beautiful'
+
+function vowelCounter(text){
+    let vowelCount = 0
+    let vowels = new Set(['a','e','i','o','u'])
+    for (let i = 0; i<text.length; i++){
+        if (vowels.has(text[i].toLowerCase())){
+            vowelCount++
+        }
+    }
+    return vowelCount
+}
+
+console.log(vowelCounter(texttest))
+
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+
+let stringArray = ['banana', 'apple', 'dragonfruit','coconut','pineapple']
+
+function mayusConvert(textArray){
+    let mayusArray = new Array()
+    for (let value of textArray){
+        mayusArray.push(value.toUpperCase())
+    }
+    return mayusArray
+}
+
+let newArray = mayusConvert(stringArray)
+console.log("Upper Case array: ", newArray)
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
 
+const testNumber = 7
+
+function isPrime(number){
+    let result = ''
+    let factors = new Set()
+    for (let factor = 1; factor<= number; factor++){
+        if (number%factor == 0){
+            factors.add(factor)
+        }
+    }
+    if ((factors.has(1)) && (factors.has(number) && (factors.size == 2))){
+        result = `${number} is a prime number`
+        return result
+    } else {
+        result = `${number} is not a prime number`
+        return result
+    }
+}
+
+console.log(isPrime(testNumber))
+
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+let array1 = [2,3,1,5,6,7,8,9,10]
+let array2 = [2,4,6,8,10,12,14,16]
+
+let commonElements = function commonElementsfinder(array1, array2){
+    const set1 = new Set (array1)
+    const set2 = new Set(array2)
+    let commonarray = []
+    set1.forEach(function (value){
+        if (set2.has(value)){
+            commonarray.push(value)
+        }
+    })
+    return commonarray
+}
+
+console.log(commonElements(array1, array2))
 
 // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
+let totalSum = 0
+
+let evenNumberSum = function evenSumCalculator(numberArray){
+    numberArray.forEach(function (value){
+        if (value%2 ==0){
+            totalSum+=value
+        }
+    })
+    return totalSum
+}
+
+console.log(`Even numbers sum for array: ${evenNumberSum(array1)}`)
 
 // 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
 
+function squared (numberArray){
+    let squaredArray = []
+    for (let value of numberArray){
+        squaredArray.push(value**2)
+    }
+    return squaredArray
+}
+
+console.log(squared(array2))
+
 // 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
 
+const nombre = 'Karen'
+
+function textInverter(text){
+    let counter = text.length
+    let invertedString = ""
+    while(counter>0){
+        invertedString +=text[counter-1]
+        counter--
+    }
+    return invertedString
+}
+
+console.log(textInverter(nombre))
+
 // 10. Crea una función que calcule el factorial de un número dado
+
+function factorialNumber(number){
+    let result = 1
+    for (let factor =1; factor<=number; factor++){
+        result*=factor
+    }
+    return result
+}
+
+console.log(factorialNumber(4))
